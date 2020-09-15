6aeb10864cc666b78dd6a74157e27804442bf0de
Alexandre Dias <alexbpdias@gmail.com>
2020-09-15 00:16:26 -0300

    Refactoring

--- lambda/refund.js
+++ lambda/refund.js
@@ -2,40 +2,39 @@ const axios = require('axios');
 
 const key = process.env.VTEX_KEY;
 const token =  process.env.VTEX_TOKEN;
-const baseUrl = process.env.BASE_URL;
+const baseURL = process.env.BASE_URL;
 const invoiceUrl = process.env.LINKEDIN_MARI;
-const errorRefund = process.env.ERROR;
+const errorRefund = process.env.ERROR_REFUND;
+const errorNotInvoiced = process.env.ERROR_NOT_INVOICED;
 
 const headers = {
   accept: 'application/json',
   'content-type': 'application/json',
   'x-vtex-api-appkey': key,
   'x-vtex-api-apptoken': token
-}
+};
+
+const instance = axios.create({
+  baseURL,
+  headers,
+  timeout: 1000
+});
   
 module.exports.handler = async (event) => {
   const id = event.currentIntent.slots['OrderID'];
   const confirmation = event.currentIntent.slots['Confirmation'];
   
-  if (confirmation === "no") {
-    return {
-      dialogAction: {
-        type: "Close",
-        fulfillmentState: "Fulfilled",
-          message: {
-              contentType: "PlainText",
-              content: "Ok, I'm canceling your request. I'll be here if you need me! :)"
-          }
-      }
-    }
+  if (confirmation === "no" || id === 'exit') {
+    const message = "Ok, I'm cancelling your request. I'll be here if you need me! :)";
+    return close(message, "Fulfilled");
   }
   else {
     try {
-      const { data } = await axios.get(`${baseUrl}/oms/pvt/orders/${id}`, { headers });
+      const { data } = await instance.get(`/orders/${id}`);
       const item = data.items[0];
       
-      await axios.post(
-        `${baseUrl}/oms/pvt/orders/${id}/invoice`,
+      await instance.post(
+        `/orders/${id}/invoice`,
         {
           type: "Input",
           issuanceDate: new Date().toISOString().split('.')[0],
@@ -45,42 +44,58 @@ module.exports.handler = async (event) => {
           items: [
             {
               id: item.uniqueId,
-              price: parseInt(item.price),
-              quantity: parseInt(item.quantity)
+              price: item.price,
+              quantity: item.quantity
             }
           ]
-        },
-        { headers }
+        }
       );
         
-      return {
-        dialogAction: {
-          type: "Close",
-          fulfillmentState: "Fulfilled",
-            message: {
-                contentType: "PlainText",
-                content: "Successfully refunded! Sorry for the inconvenience and have a nice day!"
-            }
-        }
-      }
+      const message = "Successfully refunded! Sorry for the inconvenience and have a nice day!";
+      return close(message, "Fulfilled");
+      
     } catch (error) {
       let message = "Refunded failed. Sorry for the inconvenience, please contact the support!";
       if(error.response) {
-        const errorMessage = error.response.data.error.message
-        message = errorMessage === errorRefund ? "Hmm... It seems that this order has already been refunded :)" : message
-        message = error.response.status === 404 ? "I'm not able to find your Order ID. Did you typed it correctly?": message
-      }
-      
-      return {
-        dialogAction: {
-          type: "ElicitSlot",
-          fulfillmentState: "Failed",
-            message: {
-                contentType: "PlainText",
-                content: message
-            }
+        const errorMessage = error.response.data.error.message;
+        const errorStatus = error.response.status;
+        message = errorMessage === errorRefund ? "Hmm... It seems that this order has already been refunded :)" : message;
+        message = errorMessage === errorNotInvoiced ? "Sorry, this order hasn`t been invoiced yet. Try again later" : message;
+        
+        if (errorStatus === 404) {
+          return elicit_slot("I'm not able to find your Order ID. Did you typed it correctly? Try again or type 'exit' to cancel.", "OrderID");
         }
       }
+      
+      return close(message, "Failed");
     }
   }
 };
+
+function close(message, fulfillmentState) {
+  return {
+    dialogAction: {
+      type: "Close",
+      fulfillmentState,
+        message: {
+            contentType: "PlainText",
+            content: message
+        }
+    }
+  };
+}
+
+function elicit_slot(message, slotToElicit) {
+  return {
+    dialogAction: {
+      "type": "ElicitSlot",
+      "message": {
+        "contentType": "PlainText",
+        "content": message
+      },
+      "intentName": event.currentIntent.name,
+      "slots": event.currentIntent.slots,
+      "slotToElicit": slotToElicit
+    }
+  };
+}
