dfdcaf72e1293b80de39879647750bf9b964adb1
rallysson <ralldtm@gmail.com>
2020-09-24 15:05:10 -0300

    feat: 🎸 add bot

--- store-block/react/Chatbot.tsx
+++ store-block/react/Chatbot.tsx
@@ -1,5 +1,5 @@
 //react/Chatbot.tsx
-import React from "react";
+import React, { useEffect } from "react";
 
 interface ChatbotProps {}
 
@@ -10,7 +10,25 @@ declare global {
 }
 
 const Chatbot: StorefrontFunctionComponent<ChatbotProps> = ({}) => {
-  return <iframe src="./lex-ui/index.html" />;
+  useEffect(() => {
+    (function(_d, m) {
+      var kommunicateSettings = {
+        appId: "1ecb448efd574a65337a533da9dc9f9ad",
+        popupWidget: true,
+        automaticChatOpenOnNavigation: true,
+      };
+      var s = document.createElement("script");
+      s.type = "text/javascript";
+      s.async = true;
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+      var h = document.getElementsByTagName("head")[0];
+      h.appendChild(s);
+      window.kommunicate = m;
+      m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+  }, []);
+
+  return <></>;
 };
 
 Chatbot.schema = {
